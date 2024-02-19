ad80a838b25147659149ae7dc1a6d0fbed9133db
Krishna Thakar <krishna.thakar@openxcell.com>
2024-02-19 13:49:18 +0530

    create model for user,video,like,playlist,commenet,subscription,tweet

---
+++ src/models/user.model.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    fulname: {
+      type: String,
+      required: true,
+      index: true,
+      trim: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is Required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = mongoose.model("User", userSchema);
