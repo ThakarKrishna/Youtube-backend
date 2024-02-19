b0a89630fa30dae3bf9d4d92eb03b96e814b153a
Krishna Thakar <krishna.thakar@openxcell.com>
2024-02-19 12:35:56 +0530

    install packages and connect mongoDB

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+app.use(express.json({ limit: "1kb" }));
+
+// nested object
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+
+// import routes
+
+export { app };
